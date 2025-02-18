1d17c4902aba4aa01a34975b7c4775ef6d0d3e70
Jiwook Chung <toekneechungus@Jiwooks-MacBook-Pro.local>
2025-02-18 11:28:29 -0800

    change zathura to skim

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -2,7 +2,7 @@ return   {
   "lervag/vimtex",
   lazy = false,    
   config = function()
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "skim"
     vim.cmd("set conceallevel=1")
     vim.g.tex_conceal='abdmg'
     vim.g.vimtex_quickfix_open_on_warning =0
